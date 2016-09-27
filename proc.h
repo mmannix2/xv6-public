58a2785e0b4f6064e3c030a037195ff5b13590a1
Matthew Mannix <mmannix2@mail.umw.edu>
2016-09-27 11:06:47 -0400

    Fix spelling error in comment.

--- proc.h
+++ proc.h
@@ -53,7 +53,7 @@ enum procstate { UNUSED, EMBRYO, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
 // Per-process state
 struct proc {
-  int priority; /*Added priority to implemant MLFQ Scheduler*/
+  int priority; /*Added priority to implement MLFQ Scheduler*/
   uint sz;                     // Size of process memory (bytes)
   pde_t* pgdir;                // Page table
   char *kstack;                // Bottom of kernel stack for this process
