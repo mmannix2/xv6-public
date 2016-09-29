5d256a8cd57db9f62e0af82941193c7c18f4beef
Matthew Mannix <mmannix2@mail.umw.edu>
2016-09-29 12:29:56 -0400

    Work on MLFQ.

--- proc.c
+++ proc.c
@@ -288,34 +288,40 @@ wait(void)
 void
 scheduler(void)
 {
-  struct proc *p;
+    struct proc *p;
+    //int i;
+    int priority;
+    int PRIORITY_LEVELS = 2;
+    for(priority = 0; ; priority = ((priority+1) % PRIORITY_LEVELS) ){
+        // Enable interrupts on this processor.
+        sti();
 
-  for(;;){
-    // Enable interrupts on this processor.
-    sti();
+        // Loop over process table looking for process to run.
+        acquire(&ptable.lock);
+        for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+            cprintf("Checking for priority %d.\n", priority);
+            if(p->state != RUNNABLE) {
+                cprintf("Process not runnable. Skipping.\n");
+                continue;
+            }
+            
+            if(p->priority == priority) {
+                // Switch to chosen process.  It is the process's job
+                // to release ptable.lock and then reacquire it
+                // before jumping back to us.
+                proc = p;
+                switchuvm(p);
+                p->state = RUNNING;
+                swtch(&cpu->scheduler, p->context);
+                switchkvm();
 
-    // Loop over process table looking for process to run.
-    acquire(&ptable.lock);
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->state != RUNNABLE)
-        continue;
-
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
-      swtch(&cpu->scheduler, p->context);
-      switchkvm();
-
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      proc = 0;
+                // Process is done running for now.
+                // It should have changed its p->state before coming back.
+                proc = 0;
+            }
+        }
+        release(&ptable.lock);
     }
-    release(&ptable.lock);
-
-  }
 }
 
 // Enter scheduler.  Must hold only ptable.lock
